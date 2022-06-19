19425c7f3e89b4edde347d90380c01d281d62725
Ronak Khandelwal <ronakkhandlwl@gmail.com>
2022-06-19 17:06:16 +0530

    remove bugs

--- keyboard/app.js
+++ keyboard/app.js
@@ -1,16 +1,28 @@
 let randomKey, dataKey;
-let keyId = document.getElementById("key"+randomKey);
+let keys = document.getElementsByClassName("key");
 
 const jiggleRandomKey = () => {
-    randomKey = Math.floor((Math.random()*53)+1);
-    document.getElementById("key"+randomKey).classList.add("jiggle");
-    dataKey = document.getElementById("key"+randomKey).getAttribute("data-key");
+    randomKey = Math.floor(Math.random()*53);
+    keys[randomKey].classList.add("jiggle");
+    dataKey = keys[randomKey].getAttribute("data-key");
 }
+
 document.addEventListener('keydown',function(event){
     let pressedKey = event.key.toUpperCase();
+    if(pressedKey=="TAB") {
+        event.preventDefault();
+    }
     if(pressedKey == dataKey) {
-        document.getElementById("key"+randomKey).classList.remove("jiggle");
+        keys[randomKey].classList.remove("jiggle");
         jiggleRandomKey();
+    } else {
+        if(pressedKey=='\\') {
+            pressedKey = pressedKey + pressedKey;
+        }
+        document.querySelector(`button[data-key="${pressedKey}"]`).classList.add("red");
+        setTimeout(function(){
+            document.querySelector(`button[data-key="${pressedKey}"]`).classList.remove("red");
+        },200);
     }
 });
 
