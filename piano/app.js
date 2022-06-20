7cb2b1ccc9fceb91160f5bf91d8eeeec76aade26
Ronak Khandelwal <ronakkhandlwl@gmail.com>
2022-06-20 15:50:08 +0530

    Format document

--- piano/app.js
+++ piano/app.js
@@ -1,23 +1,23 @@
 const playClip = (index) => {
-    const audio = new Audio(`audio/key-${index}.mp3`);
-    audio.play();
+  const audio = new Audio(`audio/key-${index}.mp3`);
+  audio.play();
 };
 
 const clickHandle = (dValue) => {
-    let index = 1;
-    let keys = document.querySelectorAll("path");
-    keys.forEach(keys => {
-        if(keys.getAttribute("d")==dValue) {
-            playClip(index);
-        }
-        index++;
-    });
-}
-
-window.onclick = e => {
-    element = e.target;
-    let classRef = element.getAttribute("class");
-    if(classRef=="white-keys" || classRef=="black-keys") {
-        clickHandle(element.getAttribute("d"));
+  let index = 1;
+  let keys = document.querySelectorAll("path");
+  keys.forEach((keys) => {
+    if (keys.getAttribute("d") == dValue) {
+      playClip(index);
     }
-} 
+    index++;
+  });
+};
+
+window.onclick = (e) => {
+  element = e.target;
+  let classRef = element.getAttribute("class");
+  if (classRef == "white-keys" || classRef == "black-keys") {
+    clickHandle(element.getAttribute("d"));
+  }
+};
