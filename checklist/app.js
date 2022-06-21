d5343840a7f54e65bdcb7190ad438e2b93338be8
Ronak Khandelwal <ronakkhandlwl@gmail.com>
2022-06-21 14:29:07 +0530

    Add uncheck feature

--- checklist/app.js
+++ checklist/app.js
@@ -1,25 +1,41 @@
 let checkboxes = document.querySelectorAll('input');
 let index = undefined;
-let lastChecked;
+let lastChecked, lastUnchecked;
 let isChecked = new Array(10).fill(false);
 
 const check = (idx) => {
   if (event.shiftKey) {
-    if (lastChecked != undefined) {
-      if (lastChecked > idx) {
-        let temp = idx;
-        idx = lastChecked;
-        lastChecked = temp;
+    if (isChecked[idx] == false) {
+      if (lastChecked != undefined) {
+        if (lastChecked > idx) {
+          let temp = idx;
+          idx = lastChecked;
+          lastChecked = temp;
+        }
+        for (let i = lastChecked + 1; i < idx; i++) {
+          isChecked[i] = true;
+          document.getElementById(`episode-${i + 1}`).checked = true;
+        }
       }
-
-      for (let i = lastChecked + 1; i < idx; i++) {
-        isChecked[i] = true;
-        document.getElementById(`episode-${i + 1}`).checked = true;
+    } else {
+      if (lastUnchecked != undefined) {
+        if (lastUnchecked > idx) {
+          let temp = idx;
+          idx = lastUnchecked;
+          lastUnchecked = temp;
+        }
+        for (let i = lastUnchecked + 1; i < idx; i++) {
+          isChecked[i] = false;
+          document.getElementById(`episode-${i + 1}`).checked = false;
+        }
       }
     }
   }
   if (isChecked[idx] == false) {
     lastChecked = idx;
+  }
+  if (isChecked[idx] == true) {
+    lastUnchecked = idx;
   }
   isChecked[idx] = !isChecked[idx];
 };
