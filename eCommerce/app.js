50c81652bb453e87717ccfcbf8eb5923bd1ae37b
Ronak Khandelwal <ronakkhandlwl@gmail.com>
2022-06-23 08:30:59 +0530

    Initial Commit

---
+++ eCommerce/app.js
@@ -0,0 +1,61 @@
+const menuItems = [
+  {
+    name: 'French Fries with Ketchup',
+    price: 223,
+    image: 'plate__french-fries.png',
+    alt: 'French Fries',
+    count: 0
+  },
+  {
+    name: 'Salmon and Vegetables',
+    price: 512,
+    image: 'plate__salmon-vegetables.png',
+    alt: 'Salmon and Vegetables',
+    count: 0
+  },
+  {
+    name: 'Spaghetti Meat Sauce',
+    price: 782,
+    image: 'plate__spaghetti-meat-sauce.png',
+    alt: 'Spaghetti with Meat Sauce',
+    count: 0
+  },
+  {
+    name: 'Bacon, Eggs, and Toast',
+    price: 599,
+    image: 'plate__bacon-eggs.png',
+    alt: 'Bacon, Eggs, and Toast',
+    count: 0
+  },
+  {
+    name: 'Chicken Salad with Parmesan',
+    price: 698,
+    image: 'plate__chicken-salad.png',
+    alt: 'Chicken Salad with Parmesan',
+    count: 0
+  },
+  {
+    name: 'Fish Sticks and Fries',
+    price: 634,
+    image: 'plate__fish-sticks-fries.png',
+    alt: 'Fish Sticks and Fries',
+    count: 0
+  }
+];
+
+import { addToCart } from './utility.js';
+
+const add = document.querySelectorAll('.add');
+const cart = document.querySelector('.cart-summary');
+const empty = document.querySelector('.empty');
+const subtotalDiv = document.querySelector('.amount.price.subtotal');
+const taxDiv = document.querySelector('.amount.price.tax');
+const totalDiv = document.querySelector('.amount.price.total');
+
+add.forEach((elem, idx) => {
+  elem.addEventListener('click', () => {
+    addToCart(elem, idx);
+  });
+});
+
+export { menuItems, cart, empty, subtotalDiv, taxDiv, totalDiv };
